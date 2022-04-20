0cb1c9b41b95734311f32854c8308b13f4d43af9
Arnaud Delorme <arnodelorme@gmail.com>
2022-04-19 20:32:39 -0700

    making the GUI work

--- bids2mat.m
+++ bids2mat.m
@@ -1,6 +1,6 @@
 function bids2mat(EEG,label_file,filepath)
 % this function converts a 3D matrix data to .mat samples and saves
-% the raw data, 12x12 and 6x6 interpolated grid data data in the foldername mat_files 
+% the raw data, 12x12 and 6x6 interpolated grid data data in the foldername mat_files
 %
 % EEG is the output of pop_epoch or eeg_regepochs for 1 subject and 1
 % particular task
@@ -8,17 +8,15 @@ function bids2mat(EEG,label_file,filepath)
 % label_file is the name of the .csv file where all the label info with the
 % file location will be saved.
 %
-% filepath is the absolute path to the dataset where the final dataset will reside 
+% filepath is the absolute path to the dataset where the final dataset will reside
 % for AWS s3 bucket, it could be
 % filepath = 's3://openneuro.org/ds003061'
 %
 %
 
-
-
-    if isempty(filepath)
-        filepath = pwd; %path to the dataset folder, default pwd
-    end
+if isempty(filepath)
+    filepath = pwd; %path to the dataset folder, default pwd
+end
 
 %     % delete any pre-exisiting label file with the same name
 %     if isfile(label_file)
@@ -28,74 +26,70 @@ function bids2mat(EEG,label_file,filepath)
 %         disp('Creating new lable file')
 %     end
 
-    if ~exist('mat_files','dir')
-        mkdir('mat_files')  %mat files will be written in a new folder created in the pwd
-    end
-        
-    subject_dir = ['mat_files/' EEG.subject];
-    if ~exist(subject_dir,'dir')
-        mkdir(subject_dir)
-    end
+if ~exist('mat_files','dir')
+    mkdir('mat_files')  %mat files will be written in a new folder created in the pwd
+end
 
-    eeg_dir = [subject_dir '/eeg']
-    if ~exist(eeg_dir,'dir')
-        mkdir(eeg_dir)
-    end
-     
-    
-    % get the type of epoch and any other interesting field
-    epoch_type = std_maketrialinfo([], EEG); 
-    trial_info = struct2cell(epoch_type.datasetinfo.trialinfo')';
+subject_dir = fullfile('mat_files', EEG.subject);
+if ~exist(subject_dir,'dir')
+    mkdir(subject_dir)
+end
 
-    num_samples = size(EEG.data,3);
-    for segment_num = 1:num_samples
-       filename = [ eeg_dir '/' EEG.subject '_task_' EEG.task '_run_' num2str(EEG.run) '_' num2str(segment_num) '.mat'];
+eeg_dir = fullfile(subject_dir, 'eeg');
+if ~exist(eeg_dir,'dir')
+    mkdir(eeg_dir)
+end
 
-        if isfile(filename)
-            warning('Warning: File already exisits. Skipping...')
-        else
-            
-           data = EEG.data(:,:,segment_num);
-           num_timestamps = size(data,2);
-           Z_12 = zeros(12,12,num_timestamps);
-           Z_6 = single(zeros(6,6,num_timestamps));
-            
-          parfor time_step = 1:num_timestamps
-               % topoplot_DaSh default gridscale = 12
-                [~,Z_12(:,:,time_step),~,~,~] = topoplot_DaSh(data(:,time_step)', EEG.chanlocs,  'chaninfo', EEG.chaninfo);
-%             [~,Z_6(:,:,time_step),~,~,~] = topoplot_DaSh(EEG.data(:,time_step)', EEG.chanlocs, 'whitebk', 'on', 'gridscale', 6, 'numcontour', 0,  'chaninfo', EEG.chaninfo); 
-           end
-          
 
-%%          z-score Z_12 
-%           max_z12 = max(max(max(Z_12)));
-%           min_z12 = min(min(min(Z_12)));
-%           Z_12 =  (Z_12 - min_z12)./(max_z12 -min_z12);
-%%         
-          %change from double to single precision
-          Z_12 = single(Z_12);
-          
-          %convert all NaNs to zeros
-          Z_12(isnan(Z_12))=0;
+% get the type of epoch and any other interesting field
+epoch_type = std_maketrialinfo([], EEG);
+trial_info = struct2cell(epoch_type.datasetinfo.trialinfo')';
 
-          % calculate Z_6
-          Z_6 = imresize(Z_12,0.5,'method','nearest');
+num_samples = size(EEG.data,3);
+for segment_num = 1:num_samples
+    filename = [ eeg_dir filesep EEG.subject ];
+    if ~isempty(EEG.run) filename = [ filename '_run_' num2str(EEG.run) ]; end
+    if ~isempty(EEG.session) filename = [ filename '_session_' num2str(EEG.session) ]; end
+    filename = [ filename '.mat' ];
 
-           save(filename,'data','Z_12','Z_6','-mat','-v7.3','-nocompression')
-           sample_filepath = fullfile(filepath,filename);
+    if isfile(filename)
+        warning('Warning: File already exisits. Skipping...')
+    else
 
-            %sample_file_name, event_type, segment number, participant info, original file name
-            % label_info = [sample_filepath EEG.epoch(segment_num).eventtype segment_num EEG.BIDS.pInfo(2,:) EEG.filename ];
-            if isempty(EEG.BIDS)
-                label_info = [sample_filepath trial_info(end) segment_num EEG.filename];
-            else
-                 label_info = [sample_filepath trial_info(end) segment_num EEG.BIDS.pInfo(2,:) EEG.filename];
-            end
+        data = EEG.data(:,:,segment_num);
+        num_timestamps = size(data,2);
+        Z_12 = zeros(12,12,num_timestamps);
+        Z_6 = single(zeros(6,6,num_timestamps));
 
-            writetable(cell2table(label_info),label_file,'Delimiter','tab',...
-                 'WriteMode','append','WriteRowNames',false,'WriteVariableNames',false,'QuoteStrings',true);
+        parfor time_step = 1:num_timestamps
+            % topoplot_DaSh default gridscale = 12
+            [~,Z_12(:,:,time_step),~,~,~] = topoplot_DaSh(data(:,time_step)', EEG.chanlocs,  'chaninfo', EEG.chaninfo);
+            %             [~,Z_6(:,:,time_step),~,~,~] = topoplot_DaSh(EEG.data(:,time_step)', EEG.chanlocs, 'whitebk', 'on', 'gridscale', 6, 'numcontour', 0,  'chaninfo', EEG.chaninfo);
         end
-    end
 
-   
+
+        %%          z-score Z_12
+        %           max_z12 = max(max(max(Z_12)));
+        %           min_z12 = min(min(min(Z_12)));
+        %           Z_12 =  (Z_12 - min_z12)./(max_z12 -min_z12);
+        %%
+        %change from double to single precision
+        Z_12 = single(Z_12);
+
+        %convert all NaNs to zeros
+        Z_12(isnan(Z_12))=0;
+
+        % calculate Z_6
+        Z_6 = imresize(Z_12,0.5,'method','nearest');
+
+        save(filename,'data','Z_12','Z_6','-mat','-v7.3','-nocompression')
+        sample_filepath = fullfile(filepath,filename);
+
+        %sample_file_name, event_type, segment number, participant info, original file name
+        % label_info = [sample_filepath EEG.epoch(segment_num).eventtype segment_num EEG.BIDS.pInfo(2,:) EEG.filename ];
+        label_info = [sample_filepath trial_info(end) segment_num EEG.filename];
+
+        writetable(cell2table(label_info),label_file,'Delimiter','tab',...
+            'WriteMode','append','WriteRowNames',false,'WriteVariableNames',false,'QuoteStrings',true);
+    end
 end
--- pop_bids_dl.m
+++ pop_bids_dl.m
@@ -25,12 +25,10 @@
 function pop_bids_dl(STUDY, ALLEEG, varargin)
 
 if nargin < 3
-        com = [ 'bidsFolderxx = uigetdir(''Pick an output folder'');' ...
-            'if ~isequal(bidsFolderxx, 0), set(findobj(gcbf, ''tag'', ''outputfolder''), ''string'', bidsFolderxx); end;' ...
-            'clear bidsFolderxx;' ];
-            
-    cb_eeg          = 'pop_bids_dl(''edit_eeg'', gcbf);';
-    cb_participants = 'pop_bids_dl(''edit_participants'', gcbf);';
+    com = [ 'bidsFolderxx = uigetdir(''Pick an output folder'');' ...
+        'if ~isequal(bidsFolderxx, 0), set(findobj(gcbf, ''tag'', ''outputfolder''), ''string'', bidsFolderxx); end;' ...
+        'clear bidsFolderxx;' ];
+
     cb_events       = 'pop_bids_dl(''edit_events'', gcbf);';
     uilist = { ...
         { 'Style', 'text', 'string', 'Export EEGLAB study to ML/DL format', 'fontweight', 'bold'  }, ...
@@ -44,91 +42,65 @@ if nargin < 3
         { 'Style', 'edit', 'string', '' 'tag' 'changes'  'HorizontalAlignment' 'left' 'max' 3   }, ...
         { 'Style', 'text', 'string', 'Select events to export (default all)' 'tag' 'events' 'callback' cb_events }, ...
         { 'Style', 'edit', 'string',  '' 'tag' 'events' }, ...
-        { 'Style', 'pushbutton', 'string', '...' 'callback' '' }, ...        
+        { 'Style', 'pushbutton', 'string', '...' 'callback' '' }, ...
         { 'Style', 'checkbox', 'string', 'Do not use participants ID and create anonymized participant ID instead' 'value' 1 'tag' 'newids' }, ...
         };
-    
+
     relSize = 0.7;
     geometry = { [1] [1] [1-relSize relSize*0.8 relSize*0.2] [1-relSize relSize] [1] [1] [2 1 1] [1]};
     geomvert =   [1  0.2 1                                                   1  1   3    1        1];
     userdata.ALLEEG = ALLEEG;
     userdata.STUDY  = STUDY;
-    [results,userdata,~,restag] = inputgui( 'geometry', geometry, 'geomvert', geomvert, 'uilist', uilist, 'helpcom', 'pophelp(''pop_bids_dl");', 'title', 'Export EEGLAB STUDY to BIDS for DL -- pop_bids_dl()', 'userdata', userdata );
-    if length(results) == 0, return; end
+    [results,~,~,restag] = inputgui( 'geometry', geometry, 'geomvert', geomvert, 'uilist', uilist, 'helpcom', 'pophelp(''pop_bids_dl");', 'title', 'Export EEGLAB STUDY to BIDS for DL -- pop_bids_dl()', 'userdata', userdata );
+    if isempty(results), return; end
 
     % decode some outputs
     if ~isempty(strfind(restag.license, 'CC0')), restag.license = 'CC0'; end
     options = { 'targetdir' restag.outputfolder 'License' restag.license 'comments' restag.changes 'createids' fastif(restag.newids, 'on', 'off') 'events' restag.events};
 elseif isstr(STUDY)
-    command = STUDY;
-    fig = ALLEEG;
-    userdata = get(fig, 'userdata');
-
-    switch command
-        case 'edit_events'
-            userdata.EEG = pop_participantinfo(userdata.EEG);
-        case 'edit_events'
-            userdata.EEG = pop_eventinfo(userdata.EEG);
-        case 'edit_task'
-            userdata.EEG  = pop_taskinfo(userdata.EEG);
-        case 'edit_eeg'
-            userdata.EEG = pop_eegacqinfo(userdata.EEG);
-    end
+    %     command = STUDY;
+    %     fig = ALLEEG;
+    %     userdata = get(fig, 'userdata');
+    %
+    %     switch command
+    %         case 'edit_events'
+    %             userdata.EEG = pop_eventinfo(userdata.EEG);
+    %     end
 else
     options = varargin;
 end
 
-[STUDY.BIDS_path, ~] = fileparts(STUDY.filepath);
+g = finputcheck(options, { 'targetdir' 'string'  {}   fullfile(STUDY.filepath, '..', 'ML_EXPORT'); ...
+    'License'   'string'  {}    'CC0';
+    'comments' 'string'  {}    '';
+    'events'   'string'  {}    '';
+    'createids' 'string' { 'on' 'off' } 'on' }, 'pop_bids_dl');
+if isstr(g), error(g); end
 
-addpath(genpath(STUDY.BIDS_path));
+%aws_path = ['s3://openneuro.org/' STUDY.BIDS_dataset_name];
+label_file = fullfile(g.targetdir, 'labels_s3.csv');
 
-current_path = pwd;
-if strcmp(current_path,STUDY.BIDS_path)==0
-    disp('Changing current directory to BIDS dataset folder');
-    cd(STUDY.BIDS_path);
-end
+for iSet = 1:length(ALLEEG)
+    EEG =  ALLEEG(iSet);
 
-[~,STUDY.BIDS_dataset_name,~] = fileparts(STUDY.BIDS_path) ;
-
-%Test whether the BIDS dataset has a participants.tsv and whether it is empty
-if ~isfile('participants.tsv')
-    error('Cannot find participants.tsv')
-end
-
-d = dir;
-% if a directory is empty, it will contain only '.' and '..'
-if (isfolder(STUDY.BIDS_path)==0) || (length(d)==2) 
-    error(['Cannot find ' dataset_name ' BIDS dataset, download it from OpenNeuro.org, uncompress it'])
-end
-
-if ~isfolder(fullfile([STUDY.BIDS_path, '/sub-001/eeg']))
-    error(['Cannot find subject and eeg subfolder in the BIDS dataset, download it from OpenNeuro.org, uncompress it'])
+    % check subject
+    if isempty(EEG.subject)
+        error('All datasets must have a subject ID')
+    end
     
-end
-
-
-aws_path = ['s3://openneuro.org/' STUDY.BIDS_dataset_name];
-label_file = [STUDY.BIDS_dataset_name '_labels_s3.csv'];
-
-CURRENTSTUDY = 1;
-for CURRENTSET = 1:length(ALLEEG)
-    CURRENTSET
-    EEG =  ALLEEG(CURRENTSET);
-
-    %Check for bidsevents
-    if isempty(EEG.BIDS.eInfo)
-        disp('No events file detected. Regular 2s epochs will be generated')
-        EEG = eeg_regepochs(EEG, 'limits', [0 2], 'recurrence', 2);
+    % check for bidsevents
+    if EEG.trials == 1
+        EEG = pop_resample(EEG, 128);
+        if isempty(EEG.event)
+            disp('Continuous data. Regular 2s epochs will be generated')
+            EEG = eeg_regepochs(EEG, 'limits', [0 2], 'recurrence', 2);
+        else
+            disp('Events file detected! Epochs will be generated with [-0.5, 1.5] s time window ')
+            EEG = pop_epoch(EEG, unique({EEG.event.type}), [-0.5 1.5]);
+        end
     else
-        disp('Events file detected! Epochs will be generated with [-0.5, 1.5] s time window ')
-         EEG = pop_epoch(EEG, unique({EEG.event.type}), [-0.5 1.5]);
-    end    
-
-    % if EEG.chaninfo has an empty filename field then: EEG = pop_chanedit(EEG);
-
-    if isempty(EEG.chaninfo.filename)
-        EEG = pop_chanedit(EEG, EEG.chanlocs)
+        error('Cannot process epoched datasets');
     end
 
-    bids2mat(EEG,label_file,aws_path)
+    bids2mat(EEG,label_file,g.targetdir)
 end
